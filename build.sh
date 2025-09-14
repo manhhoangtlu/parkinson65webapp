ccf0f87f291cb071c0befe682341cda21e3b82a7
manhhoangtlu <phamthihanh3363@gmail.com>
2025-09-14 09:38:09 +0700

    Cập nhật build.sh để cài đặt LFS thủ công

--- build.sh
+++ build.sh
@@ -5,9 +5,18 @@ set -o errexit
 # Cài đặt các thư viện Python
 pip install -r requirements.txt
 
-# Cài đặt Git LFS
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
-apt-get install -y git-lfs
+# Tải và cài đặt Git LFS thủ công
+# (Cách này không cần quyền admin)
+LFS_VERSION="3.5.1"
+LFS_TARBALL="git-lfs-linux-amd64-v${LFS_VERSION}.tar.gz"
+
+curl -L "https://github.com/git-lfs/git-lfs/releases/download/v${LFS_VERSION}/${LFS_TARBALL}" -o "${LFS_TARBALL}"
+tar -xzf "${LFS_TARBALL}"
+
+# Chạy script cài đặt bên trong thư mục vừa giải nén
+./install.sh
+
+# Cấu hình Git LFS
 git lfs install
 
 # Tải các tệp lớn từ LFS
