9612b5b972a08d69c971522d0e58c914bae400fd
manhhoangtlu <phamthihanh3363@gmail.com>
2025-09-14 09:42:23 +0700

    Sửa lại đường dẫn install.sh trong script build

--- build.sh
+++ build.sh
@@ -6,15 +6,25 @@ set -o errexit
 pip install -r requirements.txt
 
 # Tải và cài đặt Git LFS thủ công
-# (Cách này không cần quyền admin)
 LFS_VERSION="3.5.1"
 LFS_TARBALL="git-lfs-linux-amd64-v${LFS_VERSION}.tar.gz"
+LFS_DIR="git-lfs-${LFS_VERSION}"
 
 curl -L "https://github.com/git-lfs/git-lfs/releases/download/v${LFS_VERSION}/${LFS_TARBALL}" -o "${LFS_TARBALL}"
 tar -xzf "${LFS_TARBALL}"
 
-# Chạy script cài đặt bên trong thư mục vừa giải nén
+# --- PHẦN SỬA ĐỔI BẮT ĐẦU TỪ ĐÂY ---
+
+# Di chuyển vào thư mục vừa giải nén
+cd "${LFS_DIR}"
+
+# Chạy script cài đặt
 ./install.sh
+
+# Quay trở lại thư mục gốc của dự án
+cd ..
+
+# --- KẾT THÚC PHẦN SỬA ĐỔI ---
 
 # Cấu hình Git LFS
 git lfs install
