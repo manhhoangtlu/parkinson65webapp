31de284e1a65b2853fe59671b65e62890451c70c
manhhoangtlu <phamthihanh3363@gmail.com>
2025-09-14 09:46:50 +0700

    Cập nhật build.sh để cài đặt LFS vào thư mục cục bộ

--- build.sh
+++ build.sh
@@ -5,26 +5,17 @@ set -o errexit
 # Cài đặt các thư viện Python
 pip install -r requirements.txt
 
-# Tải và cài đặt Git LFS thủ công
+# Tải và cài đặt Git LFS thủ công vào thư mục cục bộ
 LFS_VERSION="3.5.1"
 LFS_TARBALL="git-lfs-linux-amd64-v${LFS_VERSION}.tar.gz"
-LFS_DIR="git-lfs-${LFS_VERSION}"
 
 curl -L "https://github.com/git-lfs/git-lfs/releases/download/v${LFS_VERSION}/${LFS_TARBALL}" -o "${LFS_TARBALL}"
 tar -xzf "${LFS_TARBALL}"
 
-# --- PHẦN SỬA ĐỔI BẮT ĐẦU TỪ ĐÂY ---
-
-# Di chuyển vào thư mục vừa giải nén
-cd "${LFS_DIR}"
-
-# Chạy script cài đặt
-./install.sh
-
-# Quay trở lại thư mục gốc của dự án
-cd ..
-
-# --- KẾT THÚC PHẦN SỬA ĐỔI ---
+# --- PHẦN SỬA ĐỔI ---
+# Chạy script cài đặt với tiền tố là thư mục cục bộ
+# Thay vì ./install.sh, chúng ta chỉ cần chạy git-lfs trực tiếp từ đây
+export PATH="$PWD/git-lfs-${LFS_VERSION}:$PATH"
 
 # Cấu hình Git LFS
 git lfs install
